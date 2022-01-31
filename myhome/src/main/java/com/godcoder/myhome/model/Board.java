9a1b9cf5477147bd9e36a7843a4aaf65e0299a58
theharriet <goand123@gmail.com>
2022-01-31 20:33:17 +0900

    paused

--- myhome/src/main/java/com/godcoder/myhome/model/Board.java
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -16,6 +17,7 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
+    @Size(min=2, max=30) //이방법이 편하지만 자유도의 제약이 있어서 validator만듬 -> 나는 무슨 버전이 높아서 안먹힘 다운그레이드하면 가능
     private String title;
     private String content;
 }
