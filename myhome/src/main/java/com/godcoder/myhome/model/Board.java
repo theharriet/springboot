3b74d04f609b82adc220e76d18ce7bee587c2a76
theharriet <goand123@gmail.com>
2022-02-07 23:24:04 +0900

    few

--- myhome/src/main/java/com/godcoder/myhome/model/Board.java
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -20,7 +20,4 @@ public class Board {
     @Size(min=2, max=30, message = "제목은 2자이상 30자이하임") //이방법이 편하지만 자유도의 제약이 있어서 validator만듬 -> 나는 spring 버전이 높아서 안되지만 다운그레이드하면 가능
     private String title;
     private String content;
-
-
-
 }
